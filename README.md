d501e6c976ba2fee6d06213fa171e49abfaee76a
no1harm <chen09869@gmail.com>
2018-12-27 19:59:32 +0800

    学习 TypeScript 中的 class

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 - [TypeScript 笔记「3」](https://zhuanlan.zhihu.com/p/53373648)
 
 关键字：基础类型 / any / void / 枚举 / enum / 类型转换 / toString / const / 解构赋值 / 展开语法 / ... / interface / 接口继承 / extends /
+
+- [TypeScript 笔记「4」](https://zhuanlan.zhihu.com/p/53373648)
+
+关键字：class / constructor / super / static / public / private / protected / 存取器 / getter / setter / 抽象类 / abstract / 基类 /
--- demo.ts
+++ demo.ts
@@ -19,7 +19,8 @@
     console.log(greeter(user))
 }
 
-{
+{   
+    // interface
     interface Calculator {
         (no1: number, no2: number): number
     }
@@ -52,4 +53,169 @@
 
     jack.run()
 }
-
+{
+    // class
+    class Human {
+        name:string
+        age:number
+        constructor(name:string,age:number){
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.name}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    jack.say()
+}
+{
+    // super
+    class Animal {
+        kindOf:string
+        constructor(kindOf){
+            this.kindOf = kindOf
+        }
+        move(){
+            console.log('moving...')
+        }
+    }
+    class Human extends Animal {
+        name:string
+        age:number
+        constructor(name:string,age:number){
+            super('Marine Mammal')
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.name}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    console.log(jack.kindOf)
+}
+{   
+    // static
+    class Animal {
+        kindOf:string
+        constructor(kindOf){
+            this.kindOf = kindOf
+        }
+        move(){
+            console.log('moving...')
+        }
+    }
+    class Human extends Animal {
+        static hair = 'none'
+        name:string
+        age:number
+        constructor(name:string,age:number){
+            super('Marine Mammal')
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.name}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    console.log(Human.hair)
+}
+{
+    // public
+    class Human {
+        static hair = 'none'
+        public name:string
+        public age:number
+        constructor(name:string,age:number){
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.name}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    console.log(jack.name)
+}
+{
+    // private
+    class Human {
+        static hair = 'none'
+        private name:string
+        public age:number
+        constructor(name:string,age:number){
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.name}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    jack.say()
+}
+{
+    // protected
+    class Animal {
+        protected kindOf:string
+        constructor(kindOf){
+            this.kindOf = kindOf
+        }
+    }
+    class Human extends Animal {
+        public name:string
+        public age:number
+        constructor(name:string,age:number){
+            super('Marine Mammal')
+            this.name = name
+            this.age = age
+        }
+        say(){
+            console.log(`I'm ${this.kindOf}`)
+        }
+    }
+    let jack = new Human('jack',18)
+    jack.say()
+}
+{
+    // getter/setter
+    class Human {
+        private _username:string
+        get username():string{
+            return this._username
+        }
+        set username(newValue){
+            if(newValue === "123456"){
+                this._username = 'none'
+            }else{
+                this._username = newValue
+            }
+        }
+    }
+    let jack = new Human()
+    jack.username = '123456'
+    console.log(jack.username) // 'none'
+    jack.username = 'jack'
+    console.log(jack.username) // 'jack'
+}
+{
+    // abstract
+    abstract class Animal {
+        public kindOf:string
+        abstract eat():void
+    }
+    class Human extends Animal{
+        public name:string
+        constructor(name:string){
+            super()
+            this.name = name
+        }
+        eat(){
+            console.log('rice')
+        }
+    }
+    let jack = new Human('jack')
+    jack.eat()  //"rice"
+}
